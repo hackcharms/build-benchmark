fbf25fc264eb434a602156a628b7528bbf00ed81
hackcharms <zaa78692@gmail.com>
2024-05-09 19:25:08 +0530

    both nuxt-ui and vuetify-framework

--- app.vue
+++ app.vue
@@ -1,5 +1,41 @@
+<script setup lang="ts">
+import {VCarousel, VCarouselItem} from 'vuetify/components';
+
+const items = [
+  'https://picsum.photos/1920/1080?random=1',
+  'https://picsum.photos/1920/1080?random=2',
+  'https://picsum.photos/1920/1080?random=3',
+  'https://picsum.photos/1920/1080?random=4',
+  'https://picsum.photos/1920/1080?random=5',
+  'https://picsum.photos/1920/1080?random=6'
+]
+const people = ['Wade Cooper', 'Arlene Mccoy', 'Devon Webb', 'Tom Cook', 'Tanya Fox', 'Hellen Schmidt', 'Caroline Schultz', 'Mason Heaney', 'Claudie Smitham', 'Emil Schaefer']
+
+const selected = ref(people[0])
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <UCard>
+    <UCarousel v-slot="{ item }" :items="items" :ui="{ item: 'basis-full' }" class="rounded-lg overflow-hidden" arrows>
+      <img :src="item" class="w-full" draggable="false">
+    </UCarousel>
+     <USelectMenu v-model="selected" :options="people" />
+     <v-carousel>
+  <v-carousel-item
+    src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+    cover
+  ></v-carousel-item>
+
+  <v-carousel-item
+    src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+    cover
+  ></v-carousel-item>
+
+  <v-carousel-item
+    src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+    cover
+  ></v-carousel-item>
+</v-carousel>
+  </UCard>
+
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,10 @@ export default defineNuxtConfig({
   css: ["vuetify/lib/styles/main.sass"],
   devtools: { enabled: true },
   ssr: false,
+
   build: {
     transpile: ["vuetify/lib"],
   },
+
+  modules: ["@nuxt/ui"]
 });
